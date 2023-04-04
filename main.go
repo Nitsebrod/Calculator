94992b5de1dc40f8c844032e209cb948daea8d24
Nitsebrod <tuter2000@icloud.com>
2023-04-04 19:48:25 +0300

    Добавмл GET

--- main.go
+++ main.go
@@ -3,7 +3,33 @@ package main
 import (
 	"fmt"
 	"math"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type Figures struct {
+	Name []string `json:"figuresName"`
+}
+
+var figuresData = []Figures{
+	{Name: []string{"Cone", "Cube", "Cylinder"}},
+}
+
+func getFiguresData(c *gin.Context) {
+	c.JSON(http.StatusOK, figuresData)
+}
+
+func postFiguresVolumes(c *gin.Context) {
+	// var input string
+
+	// if err := c.ShouldBindJSON(&input); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	result := measure("...")
+	c.JSON(http.StatusOK, map[string]string{"result": result})
+}
 
 type geometry interface {
 	volume() float64
@@ -36,17 +62,24 @@ func (co cone) volume() float64 {
 	return math.Pi * math.Pow(co.radius, exponent) * co.height
 }
 
-func measure(g geometry) float64 {
-	return g.volume()
+func measure(figure string) string {
+	switch figure {
+	case cone:
+
+	}
 }
 
 func main() {
-	var shape geometry
+	r := gin.Default()
+	r.GET("/get", getFiguresData)
+	r.POST("/post", postFiguresVolumes)
+	r.Run() // listen and serve on 0.0.0.0:8080
+	//var shape geometry
 	fmt.Println("Введите название фигуры:")
 	var shapeName string
 	fmt.Scan(&shapeName)
 
-	switch shapeName {
+	/*switch shapeName {
 
 	case "куб":
 		cu := cube{}
@@ -68,7 +101,7 @@ func main() {
 		shape = cy
 	default:
 		fmt.Println("Неизвестная фигура")
-	}
+	*/
 
-	fmt.Println(measure(shape))
+	//fmt.Println(measure(shape))
 }
